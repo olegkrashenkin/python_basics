cd67cf0a0ff54a750afbfc75e7a3e5e565253ac7
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2021-07-20 01:31:37 +0300

    Update task_2_5.py

--- Krashenkin_Oleg_dz_2/task_2_5.py
+++ Krashenkin_Oleg_dz_2/task_2_5.py
@@ -1,30 +1,29 @@
 price_list = [57.8, 46.51, 97, 15.3, 34, 87.15, 14.12, 22.9, 61, 11.17]
-
-
-def price_format(some_list):
-    result = ''
-    for i in some_list:
-        i = str(i)
+# Часть A:
+for i in price_list:
+    i = str(i)
+    if i == str(price_list[-1]):  # Как и в задании 2_2,3 не придумал как можно лучше сделать перенос строки
         if '.' in i:
             if '.' in i[-2:]:
-                result += i.replace('.', ' руб ') + '0 коп, '
+                print(f'{i.replace(".", " руб ")}0 коп')
             else:
-                result += i.replace('.', ' руб ') + ' коп, '
+                print(f'{i.replace(".", " руб ")} коп')
         else:
-            result += i + ' руб 00 коп, '
-        if i == str(some_list[-1]):
-            result = result[:-2] + result[-1].rstrip()
-    return result
-
-
-print(price_format(price_list))
+            print(f'{i} руб 00 коп')
+    elif '.' in i:
+        if '.' in i[-2:]:
+            print(f'{i.replace(".", " руб ")}0 коп', end=', ')
+        else:
+            print(f'{i.replace(".", " руб ")} коп', end=', ')
+    else:
+        print(f'{i} руб 00 коп', end=', ')
+# Часть B:
 id_before = id(price_list)
-print(sorted(price_list))
+price_list.sort()
+print(price_list)
 id_after = id(price_list)
 print('Сравнение id до и после сортировки =', id_before == id_after)
-sort_price_v1 = sorted(price_list, reverse=True)
-# или
-sort_price_v2 = price_list.copy()
-sort_price_v2.sort()
-sort_price_v2.reverse()
+# Часть C:
+sort_price = sorted(price_list, reverse=True)
+# Часть D:
 print(sorted(price_list, reverse=True)[4::-1])
