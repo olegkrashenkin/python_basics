8e489e1f6ae259fddfd30791a1b4f13c905f5b78
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2021-07-19 22:32:13 +0300

    Update task_2_2,3.py

--- Krashenkin_Oleg_dz_2/task_2_2,3.py
+++ Krashenkin_Oleg_dz_2/task_2_2,3.py
@@ -1,3 +1,5 @@
+# Первый вариант. Сделал до занятия, по методичке.
+# Получил список где кавычки отдельным элементом и потом убирал пробелы.
 my_list = ['в', '5', 'часов', '17', 'минут', 'температура',
            'воздуха', 'была', '+5', 'градусов']
 id_before = id(my_list)
@@ -25,4 +27,23 @@ while True:
         break
 print(result)
 id_after = id(my_list)
+print('Сравнение id до и после выполнения кода =', id_before == id_after)
+
+# Второй вариант.
+weather = ['в', '5', 'часов', '17', 'минут', 'температура',
+           'воздуха', 'была', '+5', 'градусов']
+id_before = id(weather)
+for i in weather:
+    if i.isdigit() or i.replace('+', '').isdigit():
+        if len(i) == 1:
+            print(f'"0{i}"', end=' ')
+        elif i[0] == '+':
+            print(f'"+0{i[1:]}"', end=' ')
+        else:
+            print(f'"{i}"', end=' ')
+    elif i == weather[-1]:  # Не придумал как можно лучше перенести строку в конце предложения
+        print()
+    else:
+        print(i, end=' ')
+id_after = id(weather)
 print('Сравнение id до и после выполнения кода =', id_before == id_after)
