e7adc19a79538ffaa0f164334aa2a870511ec8b2
olegkrashenkin <86842418+olegkrashenkin@users.noreply.github.com>
2021-07-20 02:19:35 +0300

    Update task_2_2,3.py

--- Krashenkin_Oleg_dz_2/task_2_2,3.py
+++ Krashenkin_Oleg_dz_2/task_2_2,3.py
@@ -4,9 +4,9 @@ id_before = id(weather)
 for i in weather:
     if i.isdigit() or i.replace('+', '').isdigit():
         if len(i) == 1:
-            print(f'"0{i}"', end=' ')
+            print(f'"{int(i):02}"', end=' ')  # Или лучше было (f'"0{i}"', end=' ')?
         elif i[0] == '+':
-            print(f'"+0{i[1:]}"', end=' ')
+            print(f'"+{int(i):02}"', end=' ')
         else:
             print(f'"{i}"', end=' ')
     elif i == weather[-1]:  # Не придумал как можно лучше перенести строку в конце предложения
